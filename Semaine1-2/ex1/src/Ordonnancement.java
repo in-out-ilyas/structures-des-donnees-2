b4071dea244ded1868bb0a62bb4e84738b6c80e5
ilooachii <ilyas.dahajbelghazou@student.vinci.be>
2025-04-03 20:11:09 +0200

    semaine1-2; ex1

--- Semaine1-2/ex1/src/Ordonnancement.java
+++ Semaine1-2/ex1/src/Ordonnancement.java
@@ -1,17 +1,35 @@
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
 public class Ordonnancement {
-	public static final int NIVEAU_PRIORITE_MAX=5;
+	public static final int NIVEAU_PRIORITE_MAX = 5;
+	private PriorityQueue<Tache> listeTaches;
 
-	public Ordonnancement(){
-
+	public Ordonnancement() {
+		listeTaches = new PriorityQueue<>((p1, p2) -> {
+			int comparaison = Integer.compare(p2.getPriorite(), p1.getPriorite());
+			if (comparaison == 0) {
+				return (p1.getDate().compareTo(p2.getDate()));
+			}
+			return comparaison;
+		});
 	}
-	public void ajouterTache (String descriptif, int priorite){
 
+	public void ajouterTache(String descriptif, int priorite) {
+		if (descriptif.isBlank() || priorite > 5 || priorite < 1) {
+			throw new IllegalArgumentException();
+		}
+		Tache newTache = new Tache(descriptif, priorite);
+		listeTaches.add(newTache);
 	}
-	
-	//renvoie la tache prioritaire
-	//renvoie null si plus de tache presente
-	public Tache attribuerTache(){
-		
-		return null;
+
+	// renvoie la tache prioritaire
+	// renvoie null si plus de tache presente
+	public Tache attribuerTache() {
+
+		return listeTaches.poll();
 	}
 }
