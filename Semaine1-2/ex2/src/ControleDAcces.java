46405f297014c237549128b7559a07332fcb4f3d
ilooachii <ilyas.dahajbelghazou@student.vinci.be>
2025-04-03 20:40:32 +0200

    semaine-2; ex2

--- Semaine1-2/ex2/src/ControleDAcces.java
+++ Semaine1-2/ex2/src/ControleDAcces.java
@@ -1,25 +1,40 @@
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class ControleDAcces {
-	
-	public ControleDAcces(){
-		
+
+	Map<Employe, Badge> listeEmployes;
+	Set<Badge> present;
+
+	public ControleDAcces() {
+		listeEmployes = new HashMap<>();
+		present = new LinkedHashSet<>();
 	}
-	
+
 	// associe le badge � un employ�
-	public void donnerBadge (Badge b, Employe e){
-	}
-	
-	// met � jour les employ�s pr�sents dans le batiment
-	public void entrerBatiment (Badge b){
+	public void donnerBadge(Badge b, Employe e) {
+		if (b==null || e==null) {
+			throw new IllegalArgumentException();
+		}
+		listeEmployes.put(e, b);
 	}
 
 	// met � jour les employ�s pr�sents dans le batiment
-	public void sortirBatiment (Badge b){
-
+	public void entrerBatiment(Badge b) {
+		present.add(b);
 	}
-	
+
+	// met � jour les employ�s pr�sents dans le batiment
+	public void sortirBatiment(Badge b) {
+		present.remove(b);
+	}
+
 	// renvoie vrai si l'employ� est dans le batiment, faux sinon
-	public boolean estDansBatiment (Employe e){
-		return false;
+	public boolean estDansBatiment(Employe e) {
+		Badge b = listeEmployes.get(e);
+		return present.contains(b);
 	}
 
 }
