3051d80777728dd68f503504b5c746e77474d1be
ilooachii <ilyas.dahajbelghazou@student.vinci.be>
2025-04-03 21:37:17 +0200

    semaine1-2; ex3

--- Semaine1-2/ex3/src/LocationPatins.java
+++ Semaine1-2/ex3/src/LocationPatins.java
@@ -1,11 +1,19 @@
 import java.time.LocalTime;
+import java.util.*;
 
 import static java.time.temporal.ChronoUnit.MILLIS;
 
 public class LocationPatins {
 	
+	private Map<Integer, Integer> listePatins;
+	private Map<Integer, LocalTime> location;
+
 	public LocationPatins(int[] casiers) {
-	
+		listePatins = new HashMap<>();
+		location = new HashMap<>();
+		for (int i = 0; i < casiers.length; i++) {
+			listePatins.put(i, casiers[i]);
+		}
 	}
 
 	// date1 < date2
@@ -18,14 +26,27 @@ public class LocationPatins {
 		if (pointure < 33 || pointure > 48)
 			throw new IllegalArgumentException();
 		LocalTime l = LocalTime.now();
-		
-		
+		for (Map.Entry<Integer, Integer> entry : listePatins.entrySet()) {
+			if (entry.getValue()==pointure && !location.containsKey(entry.getKey())) {
+				int numCasier = entry.getKey();
+				location.put(numCasier, l);
+				return numCasier;
+			}
+		}
+
+		return -1;
 		//a compl�ter
 
 	}
 
 	public double libererCasier(int numeroCasier) {
 		//a completer
+		if (location.get(numeroCasier)==null) {
+			throw new IllegalArgumentException();
+		}
+		double montant = prix(location.get(numeroCasier), LocalTime.now());
+		location.remove(numeroCasier);
+		return montant;
 	}
 
 }
