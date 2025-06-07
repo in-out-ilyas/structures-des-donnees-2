28cfe5c28a1b35c166db2b6ad06e9a60c1962c0d
ilooachii <ilyas.dahajbelghazou@student.vinci.be>
2025-06-07 15:58:54 +0200

    semaineJSP; ex5

--- Semaine1-2/ex5/src/Doodle.java
+++ Semaine1-2/ex5/src/Doodle.java
@@ -1,10 +1,14 @@
+import java.util.*;
+
 public class Doodle {
 	private PlageHoraire[] plages;
 	// a compl�ter
+	private Map<String, boolean[]> disponibilites;
 
 	public Doodle(PlageHoraire... plages) {
 		this.plages = plages;
 		// a compl�ter
+		this.disponibilites = new HashMap<>();
 	}
 
 	// ajoute les disponibilit�s d'un participant sous forme d'un tableau de booleen.
@@ -16,6 +20,8 @@ public class Doodle {
 		if (disponibilites.length != plages.length)
 			throw new IllegalArgumentException();
 		//a compl�ter
+		this.disponibilites.put(participant, disponibilites);
+
 	}
 	
 	// Hypoth�se: la PlageHoraire plage en param�tre fait bien partie du tableau plages
@@ -23,13 +29,44 @@ public class Doodle {
 	// renvoie faux si le participant n'est pas disponible ou s'il n'a pas rempli le
 	// sondage doodle
 	public boolean estDisponible(String participant, PlageHoraire plage) {
+		if (!this.disponibilites.containsKey(participant)) {
+			return false;
+		}
+
+		boolean[] disposParticipants = this.disponibilites.get(participant);
+		for (int i = 0; i < plages.length; i++) {
+			if (plages[i].equals(plage)) {
+				return disposParticipants[i];
+			}
+		}
 		return false;
 	}
 
 	// renvoie une des plages horaires qui a le maximum de participants pr�vus
 	// cette m�thode est appel�e apr�s que les participants aient rempli leurs disponibilit�s
 	public PlageHoraire trouverPlageQuiConvientLeMieux() {
-		return null;
+		if (disponibilites.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+
+		PlageHoraire max = null;
+		int maxCount = -1;
+		
+		for (int i = 0; i < plages.length; i++) {
+			int count  = 0;
+			for (boolean[] dispos : disponibilites.values()) {
+				if (dispos[i]) {
+					count ++;
+				}
+			}
+			plages[i].setNbParticipantPresent(count);
+			if (count>maxCount) {
+				maxCount=count;
+				max = plages[i];
+			}
+		}
+
+		return max;
 	}
 
 }
